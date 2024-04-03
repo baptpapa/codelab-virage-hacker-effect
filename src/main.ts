a57ef79ce6af21edd6e81b1f7e2506177b266e4a
bpapa <baptiste.papa@viragegroup.com>
2024-04-03 13:19:18 +0200

    � Initialisation of project

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
-import './style.scss'
+import "./style.scss";
 
-document.querySelector<HTMLDivElement>('#app').onmouseover= (event : MouseEvent) => {
-    
-    const letters = event?.target?.innerText?.split('');
-    let iteration =0;
-    const fun = 50
-    // event.target.innerText = Math.random();
-    const interval = setInterval(() => {
-        if(fun + letters.length < iteration){
-            clearInterval(interval);
+document.querySelector<HTMLDivElement>("#app").onmouseover = (
+  event: MouseEvent
+) => {
+  const letters = event?.target?.innerText?.split("");
+  let iteration = 0;
+  const fun = 0;
+  // event.target.innerText = Math.random();
+  const interval = setInterval(() => {
+    if (fun + letters.length < iteration) {
+      clearInterval(interval);
+    }
+    event.target.innerText = letters
+      .map((letter, index) => {
+        if (letter === " " || index < iteration) {
+          return letter;
+        } else {
+          return randomLetter();
         }
-        event.target.innerText = letters.map((letter, index) =>{
-            if(letter=== ' ' || index < iteration){
-                return letter;
-            }else{
-                return randomLetter();
-            }
-        }).join('');
-        iteration++;
-
-    }, 30)
-    
-}   
-
+      })
+      .join("");
+    iteration++;
+  }, 100);
+};
 
 function randomLetter() {
-    const n = Math.floor(Math.random()*26);
-    const alphabet = ('abcdefghijklmnopqrstuvwxyz').split("");
-    return alphabet[n]
+  const n = Math.floor(Math.random() * 26);
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return alphabet[n];
 }
